9744a21e3bcd34549265214f173c33090ec4309f
Hi-sunziyang <Hi_sunziyang@163.com>
2022-08-24 19:48:50 +0800

    完成所有子模块

--- 引用香农熵.py
+++
@@ -1,6 +0,0 @@
-import calcShannonEnt
-
-calcShannonEnt.my_dataset[0][-1] = 'maybe'
-calcShannonEnt.my_dataset[0][-2] = '0'
-calcShannonEnt.my_dataset[0][-1] = 'mayby'
-print(calcShannonEnt.my_dataset)
