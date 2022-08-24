62a3b4a9f7a3c0de1d7e0080e5472d8b0639a5d2
Hi-sunziyang <Hi_sunziyang@163.com>
2022-08-24 10:07:38 +0800

    吃透香农熵的代码

--- calcShannonEnt.py
+++ calcShannonEnt.py
@@ -1,32 +1,38 @@
 from math import log
-import operator
 
-def createDataSet():
-    dataSet = [[1, 1, 'yes'],
+
+def create_dataset():
+    dataset = [[1, 1, 'yes'],
                [1, 1, 'yes'],
                [1, 0, 'no'],
                [0, 1, 'no'],
                [0, 1, 'no']]
-    labels = ['no surfacing','flippers']
-    #更改为离散值
-    return dataSet, labels
+    labels = ['no surfacing', 'flippers']  # 更改为离散值
+    return dataset, labels
 
-def calcShannonEnt(dataSet):
-    numEntries = len(dataSet)
-    labelCounts = {}
-    for featVec in dataSet:                             #唯一元素的数量及其出现次数
-        currentLabel = featVec[-1]
-        if currentLabel not in labelCounts.keys(): labelCounts[currentLabel] = 0
-        labelCounts[currentLabel] += 1
-    shannonEnt = 0.0
-    for key in labelCounts:
-        prob = float(labelCounts[key])/numEntries
-        shannonEnt -= prob * log(prob,2)        #log base 2
-    return shannonEnt
 
-myDt,labels=createDataSet()
-print(myDt)
+def calc_shannon_ent(dataset):
+    num_entries = len(dataset)  # num_entries=5
+    label_counts = {}  # 建立一个为了计算label频数的字典
 
-a=calcShannonEnt(myDt)
-print(a)
+    for line in dataset:  # 把dataset的每一行进行输入
+        current_label = line[-1]  # -1指的是从最右边倒数第一列，current_labels=[['yes'],['yes'],['no'],['no'],['no']]
+        if current_label not in label_counts.keys():
+            label_counts[current_label] = 0
+        label_counts[current_label] += 1
 
+    print(label_counts)
+    shannon_ent = 0.0
+
+    for key in label_counts:
+        prob = float(label_counts[key]) / num_entries  # 计算概率(频数/总数）
+        shannon_ent -= prob * log(prob, 2)  # log base 2 #按照香农熵的公式计算
+        print(f"{key}的概率是{prob}")  # 单个label的概率
+    return shannon_ent
+
+
+my_dataset, my_labels = create_dataset()
+print(my_dataset)
+
+ShannonEnt = calc_shannon_ent(my_dataset)
+print(f"香农熵={ShannonEnt}.")
